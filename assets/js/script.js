f4dcde9eeabf2280b60da70fff081e462d613d33
Sushil Mohan <sushilmohan@Sushils-MacBook-Air.local>
2024-12-14 05:46:27 +0530

    added logo

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+//  Hero Banner Slick
+$('.lady-loves-banner').slick({
+    dots: true,
+    infinite: true,
+    // speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    arrows: false,
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 4000,
+  });
